faeef04ffb1a07c5189d6cc8a1308672b3706d96
Mohamed ElL FIHRY <fihrylara@gmail.com>
2023-08-15 11:33:56 +0100

    hi

--- src/components/navbox/Main.jsx
+++ src/components/navbox/Main.jsx
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 function Navebuttom() {
   return (
     <ul className="main">
-      <NavLink className="Profile" ActiveClassName='active'  to="/fihry-profile/.">
+      <NavLink className="Profile" ActiveClassName='active'  to="/">
         Profile
       </NavLink>
-      <NavLink className="Profile" to="/fihry-profile/Skills">
+      <NavLink className="Profile" to="/Skills">
         Skills
       </NavLink>
-      <NavLink className="Profile"  to="/fihry-profile/Projects">
+      <NavLink className="Profile"  to="/Projects">
         Projects
       </NavLink>
     </ul>

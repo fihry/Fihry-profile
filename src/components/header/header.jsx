56bb2ff52c5bd2265f9ef01dacf767c1ea8402b7
Mohamed ElL FIHRY <fihrylara@gmail.com>
2023-12-20 00:24:19 +0100

    ys

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { TbChevronLeft } from 'react-icons/tb';
 import './header.css';
+import { Link } from "react-router-dom";
 
-const Header = () => { 
- return (
-      <div className="header">
-          <button><TbChevronLeft /> </button>
-          
-          <h1>MOHAMED EL-FIHRY</h1>    
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to={-1}><button><TbChevronLeft /></button></Link>
+      <h1>MOHAMED EL-FIHRY</h1>
     </div>
   );
 }
